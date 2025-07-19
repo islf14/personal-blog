df9cabd81721021733e85f307238a6dc74eae86a
Is <isaiaslflores@gmail.com>
2025-07-19 10:27:49 -0500

    format

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express, { json, urlencoded } from 'express'
 import { publicRouter } from './routes/public.js'
 import { adminRouter } from './routes/admin.js'
-import { join } from 'node:path'
-// import logger from 'morgan'
 
 const app = express()
 const port = 3000
@@ -10,11 +8,9 @@ app.set('view engine', 'ejs')
 app.use(json())
 app.use(urlencoded())
 
-// app.use(logger('dev'))
-
 app.use('/', publicRouter())
 app.use('/admin', adminRouter())
-app.use(express.static(join(process.cwd(), '/public')))
+app.use(express.static('public'))
 
 app.use((req, res) => {
   res.status(404).send('<h1>404</h1>')
